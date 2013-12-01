5e2fe25d232b61cc3cab0fc1eab7e8ec860825b7
Sagar Jauhari <sagarjauhari@gmail.com>
2013-12-01 17:56:17 -0500

    replacing mu by median in plots

--- netsimile.py
+++ netsimile.py
@@ -175,7 +175,7 @@ def plot_and_save(dists, up_limit):
     figure(num=None, figsize=(12, 6), dpi=80, facecolor='w', edgecolor='k')
     plt.plot(dists, "-o")
     axhline(y=up_limit, ls='-', c='r',
-            label='Threshold: $\mu + 2\sigma = %0.2f$'%(up_limit),
+            label='Threshold: $median + 2\sigma = %0.2f$'%(up_limit),
             lw=2)
     plt.grid(True)
     plt.legend(loc='best')
